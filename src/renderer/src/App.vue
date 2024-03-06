71874d69b52d506ad7765291b5a11cfee1f7d868
Nicolas Fernandez <nicofer1404@gmail.com>
2024-03-06 17:36:20 +0100

    calculatrice

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Calculatrice/>
+  </div>
+</template>
+
+<script>
+import Calculatrice from './components/Calculatrice.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Calculatrice
+  }
+}
+</script>
