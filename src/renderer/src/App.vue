24b8b644750adae016c079f79e3720a6adf5d6b8
Nicolas Fernandez <nicofer1404@gmail.com>
2024-03-12 09:34:34 +0100

    changements CSS

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,16 +1,38 @@
 <template>
-  <div id="app">
-    <Calculatrice/>
+  <div class="calculatrice">
+    <div class="screen">{{ screen }}</div>
+    <div class="calculatrice-keys">
+      <button v-for="button in buttons" :key="button" @click="onButtonClick(button)">
+        {{ button }}
+      </button>
+    </div>
   </div>
 </template>
 
-<script>
-import Calculatrice from './components/Calculatrice.vue'
+<script setup>
+import * as math from 'mathjs'
+import { reactive, ref } from 'vue'
 
-export default {
-  name: 'App',
-  components: {
-    Calculatrice
+const screen = ref('')
+const buttons = [1, 2, 3, '+', 4, 5, 6, '-', 7, 8, 9, '*', 0, 'C', '=', '/']
+
+function onButtonClick(button) {
+  var flag = 0
+  if (button === 'C') {
+    screen.value = ''
+  } else if (button === '=') {
+    flag = 1
+    screen.value = math.evaluate(screen.value).toString()
+    console.log('Évaluation de:', screen.value)
+  } else {
+    if (flag === 1) {
+      if (button === '+' || button === '-' || button === '*' || button === '/'){
+        screen.value += button
+      } else {
+        screen.value = ''
+        screen.value += button
+      }
+    }
   }
 }
 </script>
