f5da2aebab5d2f345bcc6f39a2b6e12f266d342d
Nicolas Fernandez <nicofer1404@gmail.com>
2024-03-20 23:45:55 +0100

    amélioration css

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="calculatrice">
-    <div class="screen">{{ screen }}</div>
+    <div id="result" class="screen">{{ screen }}</div>
     <div class="calculatrice-keys">
-      <button v-for="button in buttons" :key="button" @click="onButtonClick(button)">
+      <button v-for="button in buttons" :key="button" @click="onButtonClick">
         {{ button }}
       </button>
     </div>
@@ -17,30 +17,30 @@ const screen = ref('')
 const buttons = [1, 2, 3, '+', 4, 5, 6, '-', 7, 8, 9, '*', 0, 'C', '=', '/']
 var flag = 0
 
-function onButtonClick(button) {
-  if (button === 'C') {
+function onButtonClick(event) {
+  const last_click = event.target.innerText
+  if (last_click === 'C') {
     screen.value = ''
     flag = 0
-  } else if (button === '=') {
+  } else if (last_click === '=') {
     flag = 1
     screen.value = math.evaluate(screen.value).toString()
     console.log('Évaluation de:', screen.value)
     console.log(flag)
   } else {
-    if (button === '+' || button === '-' || button === '*' || button === '/') {
-      screen.value += button
+    if (last_click === '+' || last_click === '-' || last_click === '*' || last_click === '/') {
+      screen.value += last_click
       flag = 0
     } else {
       console.log(flag)
       if (flag === 1) {
         screen.value = ''
-        screen.value += button
+        screen.value += last_click
       } else {
         flag = 0
-        screen.value += button
+        screen.value += last_click
       }
     }
   }
 }
-
 </script>
